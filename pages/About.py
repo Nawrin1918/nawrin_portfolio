6271fbc943d76548c5100ed3f1629a8959ccd980
Nawrin1918 <nawcanva00@gmail.com>
2024-12-25 03:13:50 +0600

    Add files via upload

---
+++ pages/About.py
@@ -0,0 +1,100 @@
+import streamlit as st
+st.header (' ')
+st.markdown('<p style="font-size: 28px; font-weight: bold;">Skills</p>', unsafe_allow_html=True)
+st.write("____")
+
+col4, col5= st.columns([3, 3])
+
+with col4:
+	st.markdown('<p style="font-size: 16px; font-weight: bold;">Drafting and Modelling</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 16px; font-weight: bold;">Rendering and Presentation</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 16px; font-weight: bold;">Simulation</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 16px; font-weight: bold;">Programming</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 16px; font-weight: bold;">Construction</p>', unsafe_allow_html=True)
+
+
+with col5:
+    st.markdown('<p style="font-size: 16px;">Archicad, Autocad, Rhino, Blender, Sketch up</p>', unsafe_allow_html=True)
+    st.markdown('<p style="font-size: 16px;">Adobe Photoshop, Illustrator, Lumion, Enscape</p>', unsafe_allow_html=True)
+    st.markdown('<p style="font-size: 16px;">Dialux, Ecotect</p>', unsafe_allow_html=True)
+    st.markdown('<p style="font-size: 16px;">Python</p>', unsafe_allow_html=True)
+    st.markdown('<p style="font-size: 16px;">Concrete Slab, Wood Joinery</p>', unsafe_allow_html=True)
+
+
+st.header (' ')
+st.markdown('<p style="font-size: 28px; font-weight: bold;">Work Experience</p>', unsafe_allow_html=True)
+st.write("____")
+
+col6, col7= st.columns([1, 3])
+
+	
+with col7:
+    st.markdown('<p style="font-size: 16px;">Digital rendering at Arcadian Architects</p>', unsafe_allow_html=True)
+    st.markdown('<p style="font-size: 16px;">Safe School Design at Gajnabi Road, Mohammadpur</p>', unsafe_allow_html=True)
+    st.markdown('<p style="font-size: 16px;">VP and Mentor at ANIMATA</p>', unsafe_allow_html=True)
+    st.markdown('<p style="font-size: 16px;">Animator and graphic Designer at ANIMATA</p>', unsafe_allow_html=True)
+    st.markdown('<p style="font-size: 16px;">Mentor at UDVASH Architectural Admission Courses</p>', unsafe_allow_html=True)
+
+with col6:
+	st.markdown('<p style="font-size: 16px; font-weight: bold;">2024</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 16px; font-weight: bold;">2024</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 16px; font-weight: bold;">2024</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 16px; font-weight: bold;">2024</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 16px; font-weight: bold;">2023-24</p>', unsafe_allow_html=True)
+
+
+st.header (' ')
+    
+st.markdown('<p style="font-size: 28px; font-weight: bold;">Participation</p>', unsafe_allow_html=True)
+st.write("____")
+col8, col9= st.columns([3, 3])
+
+with col8:
+	st.markdown('<p style="font-size: 16px; font-weight: bold;">Competetion Entry</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 16px; font-weight: bold;">Workshop</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 16px; font-weight: bold;">Workshop</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 16px; font-weight: bold;">Bootcamp</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 16px; font-weight: bold;">Exhibition</p>', unsafe_allow_html=True)
+
+
+with col9:
+	st.markdown('<p style="font-size: 16px;">Street Museum by Archtwist</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 16px;">Seine and Buriganga:From River to City</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 16px;">Architecture in Digital Age: Blueprints for Tomorrow</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 16px;">Orange Corners Ideation Challenge Bootcamp:2023</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 16px;">End of Term Exhibition</p>', unsafe_allow_html=True)
+    
+    
+	
+st.header (' ')
+col10, col11= st.columns([3, 3])
+
+with col10:
+	st.markdown('<p style="font-size: 24px; font-weight: bold;">Accomplishments</p>', unsafe_allow_html=True)
+	st.write("___")
+	st.markdown('<p style="font-size: 16px; font-weight: bold;">Scholarship</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 14px;">University Merit, Deans List</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 16px; font-weight: bold;">Champion</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 14px;">Orange Corners Bangladesh</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 16px; font-weight: bold;">Second Runner Up</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 14px;">Multimedia Presentation, NDNSC</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 16px; font-weight: bold;">Publication and Management</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 14px;">UDVASH Architectural Admission Guide</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 16px; font-weight: bold;">Graphic Design Course</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 14px;">DUSRA Soft Limited</p>', unsafe_allow_html=True)
+
+
+with col11:
+	st.markdown('<p style="font-size: 24px; font-weight: bold;">Co-curricular Activities</p>', unsafe_allow_html=True)
+	st.write("___")
+	st.markdown('<p style="font-size: 16px; font-weight: bold;">Co-Founder</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 14px;">Online Based Clothing Shops: Art Factory, Cuteitup, Shokhi</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 16px; font-weight: bold;">Former Volunteer</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 14px;">Give Bangladesh, Project Kombol</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 16px; font-weight: bold;">Participator</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 14px;">M&D Spring Carnival,Chand Bazaar at Aloki</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 16px; font-weight: bold;">Anchor</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 14px;">Farewell Program, Holy Cross College</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 16px; font-weight: bold;">Member</p>', unsafe_allow_html=True)
+	st.markdown('<p style="font-size: 14px;">BUET Debating Club</p>', unsafe_allow_html=True)
+   
